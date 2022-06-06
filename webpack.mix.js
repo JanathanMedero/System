1b48e2ffca95c2a1cad8299788a5fd341c176b44
Janathan Medero <medero.janathan@gmail.com>
2022-06-06 13:02:54 -0500

    Laravel

---
+++ webpack.mix.js
@@ -0,0 +1,14 @@
+const mix = require('laravel-mix');
+
+
+// mix.js('resources/js/app.js', 'public/js')
+//     .postCss('resources/css/app.css', 'public/css', [
+//         //
+//     ]);
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .browserSync({
+        proxy: 'system.test',
+       })
+    .sourceMaps();
