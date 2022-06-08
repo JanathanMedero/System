c980a9981780027235466e837831c2641b899c71
Janathan Medero <medero.janathan@gmail.com>
2022-06-08 10:54:59 -0500

    Inicio de sesión reparado

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -505,10 +505,13 @@ data-template="vertical-menu-template-free"
 						<div class="dropdown-divider"></div>
 					</li>
 					<li>
-						<a class="dropdown-item" href="auth-login-basic.html">
+						<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
 							<i class="bx bx-power-off me-2"></i>
 							<span class="align-middle">Cerrar sesión</span>
 						</a>
+						<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+							@csrf
+						</form>
 					</li>
 				</ul>
 			</li>
