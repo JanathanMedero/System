3ee5c4d1e8152420f6238e19692df90aa5484b03
Janathan Medero <medero.janathan@gmail.com>
2022-06-08 10:41:04 -0500

    vista index dashboard

--- resources/views/auth/dashboard/index.blade.php
+++ resources/views/auth/dashboard/index.blade.php
@@ -1,6 +1,38 @@
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+@extends('layouts.dashboard')
+
+@section('title')
+<title>Pyscom - Sistema</title>
+@endsection
+
+@section('content')
+
+<div class="col-lg-12">
+	<div class="row d-flex justify-content-center">
+		<div class="col-lg-8 mb-4">
+			<div class="card">
+				<div class="d-flex align-items-end row">
+					<div class="col-sm-8">
+						<div class="card-body">
+							<h5 class="card-title text-primary">Bienvenidos al sistema de ordenes e inventario de Pyscom! 🎉</h5>
+							<p class="mb-4">
+								Desde aqui podra administrar las ordenes y verificar existencias en el inventario de Pyscom.
+							</p>
+						</div>
+					</div>
+					<div class="col-sm-4 text-center text-sm-left">
+						<div class="card-body pb-0 px-0 px-md-4">
+							<img
+							src="../assets/img/illustrations/man-with-laptop-light.png"
+							height="140"
+							alt="View Badge User"
+							data-app-dark-img="illustrations/man-with-laptop-dark.png"
+							data-app-light-img="illustrations/man-with-laptop-light.png"
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,9 +5,7 @@
 	<div class="row justify-content-center">
 		<div class="col-md-8">
 			<div class="card mt-4 shadow" style="border-radius: 0px;">
-
 				<div class="card-body pt-4 pb-1 px-4">
-
 					<div class="row">
 						<div class="col-md-12 d-flex justify-content-center">
 							<h4><strong>Iniciar sesión</strong></h4>
@@ -50,59 +48,6 @@
 							</div>
 						</form>
 					</div>
-
-					{{-- <div class="row">
-						<div class="col-md-4">
-							<img src="{{ asset('images/login.svg') }}" class="img-fluid">
-						</div>
-						<div class="col-md-8 d-flex align-items-center justify-content-center">
-							<div class="row">
-								<form method="POST" action="{{ route('login') }}">
-								@csrf
-									<div class="col-md-12">
-										<div class="row mb-3">
-											<div class="col-md-4">
-												<label for="email" class="col-md-4 col-form-label text-md-end">Correo electrónico</label>
-											</div>
-
-											<div class="col-md-8">
-												<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-												@error('email')
-												<span class="invalid-feedback" role="alert">
-													<strong>{{ $message }}</strong>
-												</span>
-												@enderror
-											</div>
-										</div>
-									</div>
-
-										<div class="row mb-3">
-											<label for="password" class="col-md-4 col-form-label text-md-end">Contraseña</label>
-
-											<div class="col-md-6">
-												<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-												@error('password')
-												<span class="invalid-feedback" role="alert">
-													<strong>{{ $message }}</strong>
-												</span>
-												@enderror
-											</div>
-										</div>
-
-										<div class="row mb-0">
-											<div class="col-md-8 offset-md-4">
-												<button type="submit" class="btn btn-primary">
-													Iniciar sesión
-												</button>
-											</div>
-										</div>
-									</div>
-								</form>
-							</div>
-						</div>
-					</div> --}}
 				</div>
 			</div>
 		</div>
