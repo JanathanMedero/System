0bdf84a0e434223cbe7837b74d371fadb9907a64
Janathan Medero <medero.janathan@gmail.com>
2022-08-02 10:37:52 -0500

    Tabla de ventas

--- app/Http/Livewire/ShowSales.php
+++ app/Http/Livewire/ShowSales.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Sale;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShowSales extends Component
 {
+    use WithPagination;
+
+    public $search = "";
+
     public function render()
     {
-        return view('livewire.show-sales');
+        $sales = Sale::where('brand', 'like', '%' . $this->search . '%')->orderBy('created_at', 'DESC')
+        ->orWhere('description', 'like', '%' . $this->search . '%')
+        ->orWhere('id', 'like', '%' . $this->search . '%')
+        ->paginate(10);
+
+        return view('livewire.show-sales', compact('sales'));
     }
 }
--- resources/views/livewire/show-sales.blade.php
+++ resources/views/livewire/show-sales.blade.php
@@ -10,7 +10,7 @@
                     <div class="col-md-12 px-4">
                         <div class="input-group input-group-merge">
                             <span class="input-group-text" id="basic-addon-search31"><i class="bx bx-search"></i></span>
-                            <input type="number" wire:model="search" class="form-control" placeholder="Buscar orden de venta (Ingrese el no. de orden)... "aria-label="Search..."/>
+                            <input type="text" wire:model="search" class="form-control" placeholder="Buscar orden de venta (Ingrese el no. de orden)... "aria-label="Search..."/>
                         </div>
                     </div>
                 </div>
@@ -29,60 +29,24 @@
                             <th>Fecha de venta</th>
                         </tr>
                     </thead>
-                    {{-- <tbody>
+                    <tbody>
 
-                        @forelse($orders as $order)
+                        @forelse($sales as $sale)
 
                         <tr>
-                            <td><strong>{{ $order->id }}</strong></td>
-                            <td>{{ $order->client->name }}</td>
-                            <td>{{ $order->created_at->diffForHumans() }}</td>
-                            <td>
-                                @if($order->office_id == 1)
-                                <span class="badge bg-primary">Sucursal Matriz</span>
-                                @else
-                                <span class="badge bg-warning">Sucursal Virrey</span>
-                                @endif
-                            </td>
-                            <td>
-                                @if($order->status == 'active')
-                                <span class="badge bg-success">Activa</span>
-                                @else
-                                <span class="badge bg-danger">Cancelada</span>
-                                @endif
-                            </td>
-                            <td>
-                                <div class="d-flex justify-content-around">
-                                    <div>
-                                        <a href="{{ route('serviceOrder.show', $order->id) }}" type="button" class="btn rounded-pill btn-info"> <span class="tf-icons bx bx-show"></span>&nbsp;Mostrar orden</a>
-                                    </div>
-                                    <div>
-
-                                        @if($order->status == 'active')
-                                        <form class="form-delete mx-2" action="{{ route('serviceOrder.updateStatus', $order->id) }}" method="POST">
-                                            @csrf
-                                            <button type="submit" class="btn rounded-pill btn-danger">
-                                                <span class="tf-icons bx bx-trash"></span>&nbsp; Cancelar orden
-                                            </button>
-                                        </form>
-                                        @else
-                                        <form class="form-active mx-2" action="{{ route('serviceOrder.updateStatus', $order->id) }}" method="POST">
-                                            @csrf
-                                            <button type="submit" class="btn rounded-pill btn-success">
-                                                <span class="tf-icons bx bx-check"></span>&nbsp; Activar orden
-                                            </button>
-                                        </form>
-                                        @endif
-                                    </div>
-                                </div>
-                            </td>
+                            <td><strong>{{ $sale->id }}</strong></td>
+                            <td>{{ $sale->brand }}</td>
+                            <td>{{ $sale->quantity }}</td>
+                            <td>{{ $sale->category }}</td>
+                            <td>{{ $sale->office }}</td>
+                            <td>{{ $sale->created_at->diffForHumans() }}</td>
                         </tr>
                         @empty
                         <tr>
                             <td colspan="5"><h3 class="mb-0 text-center"><strong>No se encontró ningúna orden de servicio</strong></h3></td>
                         </tr>
                         @endforelse
-                    </tbody> --}}
+                    </tbody>
                 </table>
             </div>
         </div>
