caf4daac14fecbd39ec8dd9f64b94daa98e1ec76
unknown <medero.janathan@gmail.com>
2022-08-10 12:07:14 -0500

    Bug de fecha en modal de ventas reparado

--- resources/views/partials/show-sale.blade.php
+++ resources/views/partials/show-sale.blade.php
@@ -7,24 +7,16 @@
             </div>
             <div class="modal-body">
                 <div class="row mb-3">
-                    <div class="col-4">
+                    <div class="col-6">
                         <label for="brand" class="form-label">Marca</label>
                         <input type="text" id="brand" class="form-control" name="brand" placeholder="Ingrese la marca" value="{{ $sale->brand }}" readonly />
                     </div>
-                    <div class="col-4">
-                        <label for="date" class="form-label">Fecha de venta</label>
-                        <input type="date" id="date" class="form-control" name="date" placeholder="Ingrese una breve descripción" value="{{ $sale->created_at }}" readonly />
-                    </div>
-                    <div class="col-4">
+                    <div class="col-6">
                         <label for="public_price" class="form-label">Precio público</label>
                         <input type="number" id="public_price" class="form-control" name="public_price" placeholder="Ingrese el precio al público" value="{{ $sale->public_price }}" readonly />
                     </div>
                 </div>
                 <div class="row mb-3">
-                    <div class="col-4">
-                        <label for="sell" class="form-label">Fecha de venta</label>
-                        <input type="date" id="sell" class="form-control" placeholder="Ingrese las existencias en matriz" value="{{ date('d-m-Y', strtotime($sale->created_at)); }}" readonly />
-                    </div>
                     <div class="col-4">
                         <label for="quantity" class="form-label">unidades vendidas</label>
                         <input type="number" id="quantity" class="form-control" name="quantity" placeholder="Ingrese las existencias totales" value="{{ $sale->quantity }}" readonly />
@@ -32,6 +24,10 @@
                     <div class="col-4">
                         <label for="category" class="form-label">Categoría</label>
                         <input type="text" id="category" class="form-control" name="category" value="{{ $sale->category }}" readonly />
+                    </div>
+                    <div class="col-4">
+                        <label for="sell" class="form-label">Fecha de venta</label>
+                        <input class="form-control" type="date" name="date_of_sale" id="date_of_sale" value="{{ date('Y-m-d', strtotime($sale->created_at)) }}" readonly>
                     </div>
                 </div>
                 <div class="row mb-3">
