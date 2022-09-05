14f6134bdbb79fb4e6c5aa966e003d184780ff81
unknown <medero.janathan@gmail.com>
2022-09-05 11:17:48 -0500

    Seeder de empleados

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -44,75 +44,75 @@ class DatabaseSeeder extends Seeder
             'password'  => Hash::make('webmaster.pyscom2021'),
         ]);
 
-        // DB::table('users')->insert([
-        //     'role_id'   => $RolAdministrador->id,
-        //     'name'      => 'Jose Alberto Avalos Villaseñor',
-        //     'email'     => 'gerencia@pyscom.com',
-        //     'password'  => Hash::make('gerencia.pyscom.2021'),
-        // ]);
+        DB::table('users')->insert([
+            'role_id'   => $RolAdministrador->id,
+            'name'      => 'Jose Alberto Avalos Villaseñor',
+            'email'     => 'gerencia@pyscom.com',
+            'password'  => Hash::make('gerencia.pyscom.2021'),
+        ]);
 
-        // DB::table('users')->insert([
-        //     'role_id'   => $RolAdministrador->id,
-        //     'name'      => 'Catalina Alcocer',
-        //     'email'     => 'ventas@pyscom.com',
-        //     'password'  => Hash::make('ventas.pyscom2021'),
-        // ]);
+        DB::table('users')->insert([
+            'role_id'   => $RolAdministrador->id,
+            'name'      => 'Catalina Alcocer',
+            'email'     => 'ventas@pyscom.com',
+            'password'  => Hash::make('ventas.pyscom2021'),
+        ]);
 
-        // DB::table('users')->insert([
-        //     'role_id'   => $RolEmpleado->id,
-        //     'name'      => 'Ana Manuela Bautista Cruz',
-        //     'email'     => 'pyscom@live.com.mx',
-        //     'password'  => Hash::make('ana.pyscom2021'),
-        // ]);
+        DB::table('users')->insert([
+            'role_id'   => $RolEmpleado->id,
+            'name'      => 'Ana Manuela Bautista Cruz',
+            'email'     => 'pyscom@live.com.mx',
+            'password'  => Hash::make('ana.pyscom2021'),
+        ]);
 
-        // DB::table('users')->insert([
-        //     'role_id'   => $RolEmpleado->id,
-        //     'name'      => 'Eduardo de la Cruz',
-        //     'email'     => 'sistemas@pyscom.com',
-        //     'password'  => Hash::make('sistemas.pyscom2021'),
-        // ]);
+        DB::table('users')->insert([
+            'role_id'   => $RolEmpleado->id,
+            'name'      => 'Eduardo de la Cruz',
+            'email'     => 'sistemas@pyscom.com',
+            'password'  => Hash::make('sistemas.pyscom2021'),
+        ]);
 
-        // DB::table('users')->insert([
-        //     'role_id'   => $RolEmpleado->id,
-        //     'name'      => 'Sinuhé Daniel Velzquez',
-        //     'email'     => 'logistica@pyscom.com',
-        //     'password'  => Hash::make('logistica.pyscom2021'),
-        // ]);
+        DB::table('users')->insert([
+            'role_id'   => $RolEmpleado->id,
+            'name'      => 'Sinuhé Daniel Velzquez',
+            'email'     => 'logistica@pyscom.com',
+            'password'  => Hash::make('logistica.pyscom2021'),
+        ]);
 
-        // DB::table('users')->insert([
-        //     'role_id'   => $RolEmpleado->id,
-        //     'name'      => 'Stefani Rangel',
-        //     'email'     => 'ventasvirrey@pyscom.com',
-        //     'password'  => Hash::make('ventas.virrey.pyscom.2021'),
-        // ]);
+        DB::table('users')->insert([
+            'role_id'   => $RolEmpleado->id,
+            'name'      => 'Stefani Rangel',
+            'email'     => 'ventasvirrey@pyscom.com',
+            'password'  => Hash::make('ventas.virrey.pyscom.2021'),
+        ]);
 
-        // DB::table('users')->insert([
-        //     'role_id'   => $RolEmpleado->id,
-        //     'name'      => 'Jessica Hernandez Pille',
-        //     'email'     => 'adminvirrey@pyscom.com',
-        //     'password'  => Hash::make('adminvirrey.pyscom.2021'),
-        // ]);
+        DB::table('users')->insert([
+            'role_id'   => $RolEmpleado->id,
+            'name'      => 'Jessica Hernandez Pille',
+            'email'     => 'adminvirrey@pyscom.com',
+            'password'  => Hash::make('adminvirrey.pyscom.2021'),
+        ]);
 
-        // DB::table('users')->insert([
-        //     'role_id'   => $RolEmpleado->id,
-        //     'name'      => 'María Monica Aavalos Villaseñor',
-        //     'email'     => 'gerenciavirrey@pyscom.com',
-        //     'password'  => Hash::make('gerenciavirrey.pyscom2021'),
-        // ]);
+        DB::table('users')->insert([
+            'role_id'   => $RolEmpleado->id,
+            'name'      => 'María Monica Aavalos Villaseñor',
+            'email'     => 'gerenciavirrey@pyscom.com',
+            'password'  => Hash::make('gerenciavirrey.pyscom2021'),
+        ]);
 
-        // DB::table('users')->insert([
-        //     'role_id'   => $RolEmpleado->id,
-        //     'name'      => 'Homero Patricio Acosta Pérez',
-        //     'email'     => 'tivirrey@pyscom.com',
-        //     'password'  => Hash::make('tivirrey.pyscom2021'),
-        // ]);
+        DB::table('users')->insert([
+            'role_id'   => $RolEmpleado->id,
+            'name'      => 'Homero Patricio Acosta Pérez',
+            'email'     => 'tivirrey@pyscom.com',
+            'password'  => Hash::make('tivirrey.pyscom2021'),
+        ]);
 
-        // DB::table('users')->insert([
-        //     'role_id'   => $RolEmpleado->id,
-        //     'name'      => 'Edgar García Gómez',
-        //     'email'     => 'edgar@pyscom.com',
-        //     'password'  => Hash::make('edgar.pyscom2021'),
-        // ]);
+        DB::table('users')->insert([
+            'role_id'   => $RolEmpleado->id,
+            'name'      => 'Edgar García Gómez',
+            'email'     => 'edgar@pyscom.com',
+            'password'  => Hash::make('edgar.pyscom2021'),
+        ]);
 
         // DB::table('categories')->insert([
         //     'name' => 'Toner',
