397938e455e44fd27fd1a07eb06b8a867ddaa44b
unknown <medero.janathan@gmail.com>
2022-09-06 10:38:45 -0500

    Errores responsivos en formulario de editar producto en inventario

--- resources/views/livewire/edit.blade.php
+++ resources/views/livewire/edit.blade.php
@@ -30,11 +30,11 @@
                                 <label for="dealer_price" class="form-label">Precio distribuidor</label>
                                 <input type="number" id="dealer_price" class="form-control" name="dealer_price" placeholder="Ingrese el precio de distribuidor" onClick="this.select();" wire:model="dealer_price" min="1"/>
                             </div>
-                            <div class="col-6 mt-2 col-md-4 mt-md-0">
+                            <div class="col-12 col-sm-6 mt-2 col-md-4 mt-md-0">
                                 <label for="stock_matriz" class="form-label">Existencias en matriz</label>
                                 <input type="number" min="0" id="stock_matriz" class="form-control" placeholder="Ingrese las existencias en matriz" onClick="this.select();" wire:model="stock_matriz" required />
                             </div>
-                            <div class="col-6 mt-2 col-md-4 mt-md-0">
+                            <div class="col-12 col-sm-6 mt-2 col-md-4 mt-md-0">
                                 <label for="stock_virrey" class="form-label">Existencias en virrey</label>
                                 <input type="number" id="stock_virrey" class="form-control" placeholder="Ingrese las existencias en virrey" min="0" onClick="this.select();"  wire:model="stock_virrey" required />
                             </div>
@@ -55,12 +55,12 @@
                         </div>
 
                         <div class="row mb-3">
-                            <div class="col-6 col-md-4">
+                            <div class="col-12 col-md-4">
                                 <label for="stock_total" class="form-label">Existencias totales</label>
                                 <input type="number" id="stock_total" class="form-control" name="stock_total" placeholder="Ingrese las existencias totales" wire:model="stock_total" value="{{ $stock_matriz + $stock_virrey }}" min="0" required disabled />
                             </div>
 
-                            <div class="col-6 col-md-4">
+                            <div class="col-12 col-md-4">
                                 <label for="gain_public" class="form-label">Ganancia a público</label>
                                 <input type="number" disabled id="gain_public" class="form-control" name="gain_public" placeholder="Ingrese la ganancia al público" wire:model="gain_public" min="0" step="0.01" required />
                             </div>
